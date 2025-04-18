471121a2dbddcca2e37d69a05708e880ac622db9
vivekrk44 <vivekrk44@gmail.com>
2025-04-18 19:12:43 +0400

    feat(plugins): add auto-session plugin for session management
    Added the 'rmagatti/auto-session' plugin with configuration options to suppress specific directories. This enhances session management capabilities in Neovim.

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,6 +1,19 @@
 return {
 
   {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+      }
+  },
+
+  {
     "nvim-telescope/telescope-ui-select.nvim",
     event = "VeryLazy",
     dependencies = { "nvim-telescope/telescope.nvim" },
